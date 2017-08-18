25aa550671d43c1b0ca20c4e3259eecbc48494ed
Alfonso Farruggia <alfarruggia@gmail.com>
2017-08-18 16:30:05 +0200

    add "code" to response message

--- config/code.js
+++ config/code.js
@@ -2,18 +2,18 @@
  * Created by alfonso on 16/08/17.
  */
 var code=[];
-code[200]={mess:"OK"};
-code[201]={mess:"Created"};
-code[204]={mess:"No Content"};
+code[200]={mess:"OK",code:200};
+code[201]={mess:"Created",code:201};
+code[204]={mess:"No Content",code:204};
 
-code[304]={mess:"Not Modified"};
+code[304]={mess:"Not Modified",code:304};
 
-code[400]={mess:"Bad Request"};
-code[401]={mess:"Unauthorized"};
-code[403]={mess:"Forbidden"};
-code[404]={mess:"Not Found"};
-code[409]={mess:"Conflict"};
+code[400]={mess:"Bad Request",code:400};
+code[401]={mess:"Unauthorized",code:401};
+code[403]={mess:"Forbidden",code:403};
+code[404]={mess:"Not Found",code:404};
+code[409]={mess:"Conflict",code:409};
 
-code[500]={mess:"Internal Server Error"};
+code[500]={mess:"Internal Server Error",code:500};
 
 module.exports=code;
